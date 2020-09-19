febd458bcbec3d20af2495b95e064b8ae4c416f0
bkanister <b.kanister@gmail.com>
2020-09-19 21:40:29 +0300

    feat: complete what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!'
+  } else if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error('THROWN')
+  } else {
+    const month = date.getMonth()
+    switch (true) {
+      case month >= 11 || month <= 1:
+        return 'winter'
+      case month >= 2 && month <= 4:
+        return 'spring'
+      case month >= 5 && month <= 7:
+        return 'summer'
+      case month >=8 && month <= 10:
+        return 'autumn'
+    }
+  }
 };
