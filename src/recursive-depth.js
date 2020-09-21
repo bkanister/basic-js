049bea4475a1e573f41e286edcd8ba070605b2d1
bkanister <b.kanister@gmail.com>
2020-09-21 21:00:14 +0300

    feat: add depth task with one test failing

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(depth = 1) {
+    this.depth = depth
+  }
+  calculateDepth(arr) {
+    if (arr.filter(el => Array.isArray(el)).length === 0) {
+      const final = this.depth
+      this.depth = 1
+      return final
+    } else {
+      this.depth++
+      return this.calculateDepth(arr.filter(el => Array.isArray(el))[0])
+    }
   }
 };
