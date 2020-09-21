151898fcda7cd51c2286d1320c7a7c12586364dd
bkanister <b.kanister@gmail.com>
2020-09-21 14:00:57 +0300

    feat: complete simple chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push(`( ${' '} )`)
+      return this
+    } else {
+      this.chain.push(`( ${value} )`)
+      return this
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.chain[position]) {
+      this.chain = []
+      throw new Error()
+    } else {
+      this.chain.splice(position-1, 1)
+      return this
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const completed = this.chain.join('~~')
+    this.chain = []
+    return completed
   }
 };
 
